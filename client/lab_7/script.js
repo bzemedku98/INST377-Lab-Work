e4c2500ec0eb348546cf03d6f5d1a5d49c97c17f
Biniam Zemedkun <ccobra230@gmail.com>
2022-11-03 23:42:02 -0400

    added filter function and changed button name

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -47,7 +47,6 @@ function injectHTML(list) {
 function processRestaurants(list) {
   console.log('fired restaurants list');
   const range = [...Array(15).keys()];
-  // eslint-disable-next-line no-unused-vars
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];
@@ -72,6 +71,15 @@ function processRestaurants(list) {
       - Return only their name, category, and location
       - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
     */
+}
+
+function filterList(list, filterInputValue) {
+  const newArray = list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = filterInputValue.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+  return newArray;
 }
 
 async function mainEvent() {
@@ -112,31 +120,39 @@ async function mainEvent() {
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+  // the question mark in this means "if this is set at all"
+  if (arrayFromJson.data?.length > 0) { return; } // Return if we have no data
+  submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
-    // Let's hide our load button not that we have some data to manipulate
-    loadAnimation.classList.remove('lds-ellipsis');
-    loadAnimation.classList.add('lds-ellipsis_hidden'); // Let's turn the submit button back on by setting to display as a block when we have data available
+  // Let's hide our load button not that we have some data to manipulate
+  loadAnimation.classList.remove('lds-ellipsis');
+  loadAnimation.classList.add('lds-ellipsis_hidden'); // Let's turn the submit button back on by setting to display as a block when we have data available
+  
+  let currentList = [];
+  
+  form.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+    const filteredList = filterList(currentList);
+    injectHTML(filteredList);
+  });
 
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
-      submitEvent.preventDefault();
+  // And here's an eventListener! It's listening for a "submit" button specifically being clicked
+  // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+  form.addEventListener('submit', (submitEvent) => {
+    // This is needed to stop our page from changing to a new URL even though it heard a GET request
+    submitEvent.preventDefault();
 
-      // This constant will have the value of your 15-restaurant collection when it processes
-      const restaurantList = processRestaurants(arrayFromJson.data);
-      // console.log(restaurantList);
+    // This constant will have the value of your 15-restaurant collection when it processes
+    currentList = processRestaurants(arrayFromJson.data);
+    console.log(currentList);
 
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
+    // And this function call will perform the "side effect" of injecting the HTML list for you
+    injectHTML(currentList);
 
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
-    });
-  }
+    // By separating the functions, we open the possibility of regenerating the list
+    // without having to retrieve fresh data every time
+    // We also have access to some form values, so we could filter the list based on name
+  });
 }
 
 /*
